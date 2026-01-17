0d22422e5dcec90c553ffe0359204b1437273dc5
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-01-18 00:00:53 +0100

    undo tab replacement

--- SpriterDotNet/Providers/SnapshotFrameDataProvider.cs
+++ SpriterDotNet/Providers/SnapshotFrameDataProvider.cs
@@ -20,10 +20,10 @@ namespace SpriterDotNet.Providers
         {
             Dictionary<string, FrameData[]> results = new Dictionary<string, FrameData[]>();
 
-			if (entity.Animations == null)
-				return results;
+            if (entity.Animations == null)
+                return results;
 
-			foreach (SpriterAnimation anim in entity.Animations)
+            foreach (SpriterAnimation anim in entity.Animations)
             {
                 int length = (int)Math.Ceiling(anim.Length / interval);
                 FrameData[] animData = new FrameData[length];
